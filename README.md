c69b11ee222fc39eb187f11ba7f664e53c34db64
Randi142 <randi142@gmail.com>
2021-05-28 01:54:09 +0800

    ver 1.0

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 An auto clicker for Princess Connect on BlueStacks
 
 ## How to use
-Press Ctrl+1 to Ctrl+5 to activate the auto clicker. The number represents the position of the character.
+Press Ctrl+1 to Ctrl+5 to activate the auto clicker. The numbers represent the position of characters.
 
 Press Ctrl+\` to stop the auto clicker.
 
-***The windows size of BlueStacks will resize to 1600*900 automatically.**
--- autoclicker.cpp
+++ autoclicker.cpp
@@ -1,79 +1,80 @@
 #include <iostream>
 #include <windows.h>
+#include <thread>
+#define KEY 0x8000
+#define x_ratio 0.25
+#define y_ratio 0.825
 
-void windowAdjust(HWND &hwnd, RECT &rect, int &x, int &y);
-void keyCheck(int &flag);
-void autoClick(int flag, int x, int y);
+void keyCheck(int &key);
+void autoClick(int &key);
+RECT rect;
+HWND hwnd = FindWindowA(NULL,"BlueStacks");
+int first_xPos, first_yPos;
+int console_xPos, console_yPos;
+int width, length;
 
 int main(){
+    int key;
+    if(hwnd == NULL)
+        std::cout<<"Cannot find BlueStacks"<<std::endl;
+    else
+    {
+        std::thread key_check_thread(keyCheck, std::ref(key));
+        std::thread clicking_thread(autoClick, std::ref(key));
+        key_check_thread.join();
+        clicking_thread.join();
+        return 0;
+    }
+}
 
-    int xPos, yPos;
-    int flag;
-    HWND hwnd = FindWindowA(NULL,"BlueStacks");
-    RECT rect;
-
-    SetWindowPos(hwnd ,0,0,0 ,1600,900 ,SWP_SHOWWINDOW|SWP_NOMOVE);
-    if(hwnd==NULL)
-        std::cout<<"cannot find bluestacks"<<std::endl;
-    else{
-        std::cout<<"auto clicker is ready"<<std::endl;
-        while (1)
-        {   
-            windowAdjust(hwnd, rect, xPos, yPos);
-            keyCheck(flag);
-            autoClick(flag,xPos,yPos);
-        } 
-    }
-    return 0;
-}
-//adjust window position,size&scale
-void windowAdjust(HWND &hwnd, RECT &rect, int &x, int &y){
-    GetWindowRect(hwnd,&rect);
-    x = rect.left;
-    y = rect.top;
-}
-//check keyboard from Ctrl+` to Ctrl+5
-void keyCheck(int &flag){
-    if((GetAsyncKeyState(VK_CONTROL) & 0x8000) && (GetAsyncKeyState(0x31) & 0x8000)){
-        flag = 1;
-    }
-    else if((GetAsyncKeyState(VK_CONTROL) & 0x8000) && (GetAsyncKeyState(0x32) & 0x8000)){
-        flag = 2;
-    }
-    else if((GetAsyncKeyState(VK_CONTROL) & 0x8000) && (GetAsyncKeyState(0x33) & 0x8000)){
-        flag = 3;
-    }
-    else if((GetAsyncKeyState(VK_CONTROL) & 0x8000) && (GetAsyncKeyState(0x34) & 0x8000)){
-        flag = 4;
-    }
-    else if((GetAsyncKeyState(VK_CONTROL) & 0x8000) && (GetAsyncKeyState(0x35) & 0x8000)){
-        flag = 5;
-    }
-    else if((GetAsyncKeyState(VK_CONTROL) & 0x8000) && (GetAsyncKeyState(VK_OEM_3) & 0x8000)){
-        flag = 0;
-    }
-}
-//auto click according to the order
-void autoClick(int flag, int x, int y){
-    if(flag){
-        switch(flag){
-            case 1:
-                SetCursorPos(x+390, y+745);
-                break;
-            case 2:
-                SetCursorPos(x+580, y+745);
-                break;
-            case 3:
-                SetCursorPos(x+770, y+745);
-                break;
-            case 4:
-                SetCursorPos(x+960, y+745);
-                break;
-            case 5:
-                SetCursorPos(x+1150, y+745);
-                break;
+//check keyboard state from Ctrl+1 to Ctrl+5 and Ctrl+`
+void keyCheck(int &key){
+    int num = 0x30;
+    int add = 0;
+    while(true)
+        if((GetAsyncKeyState(VK_CONTROL) & KEY)){    //check ctrl
+            if(GetAsyncKeyState(num+add) & KEY)      //check 1-5
+                key = add;
+            if(GetAsyncKeyState(VK_OEM_3) & KEY)     //check `
+                key = 0;
+            add++;
+            if(add == 6)
+                add = 0;
         }
-        mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-        Sleep(10);
-    }
+}
+
+//auto click according to the order
+void autoClick(int &key){
+    while(true)
+        if(key){
+            switch(key){
+                case 1:
+                    SetCursorPos(first_xPos+console_xPos, first_yPos+console_yPos);
+                    break;
+                case 2:
+                    SetCursorPos(first_xPos*1.5+console_xPos, first_yPos+console_yPos);
+                    break;
+                case 3:
+                    SetCursorPos(first_xPos*2+console_xPos, first_yPos+console_yPos);
+                    break;
+                case 4:
+                    SetCursorPos(first_xPos*2.5+console_xPos, first_yPos+console_yPos);
+                    break;
+                case 5:
+                    SetCursorPos(first_xPos*3+console_xPos, first_yPos+console_yPos);
+                    break;
+            }
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            Sleep(10);
+        }
+        else{
+            //update console position & size
+            GetWindowRect(hwnd,&rect);
+            console_xPos = rect.left;
+            console_yPos = rect.top;
+            width = rect.bottom - rect.top;
+            length = rect.right - rect.left;
+            first_xPos = length*x_ratio;
+            first_yPos = width*y_ratio; 
+        }
 }
